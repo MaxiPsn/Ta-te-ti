9528161e687130c7dda55c1a0e11c47c14bfbaed
falgirikkven <35975586+falgirikkven@users.noreply.github.com>
2018-03-11 23:08:12 -0300

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,54 +1,44 @@
--- Tablero
-local tablero = {}
-tablero.arriba_izquierda_x = 140
-tablero.arriba_izquierda_y = 100
-tablero.ancho = 100
-tablero.alto = 60
-tablero.distancia_horizontal = 20
-tablero.distancia_vertical = 30
-tablero.celdas = {}
+--[[
+*** variables_globales ***
+total_alto    (rango Y del canvas)
+total_ancho   (rango X del canvas)
+*** fin variables_globales ***
+]]--
 
-function tablero.nueva_celda( x, y )
-   return( { pos_x = x,
-	         pos_y = y,
-	         alto = tablero.alto,
-	         ancho = tablero.ancho } )
-end
+mensaje = ""
 
-function tablero.load()
-  local total_ancho = love.graphics.getWidth( )
-  local total_alto = love.graphics.getHeight( )
-  tablero.arriba_izquierda_x = total_ancho * 0.15
-  tablero.arriba_izquierda_y = total_alto * 0.15
-  tablero.ancho = total_ancho * 0.20
-  tablero.alto = total_alto * 0.20
-  tablero.distancia_horizontal = total_ancho * 0.05
-  tablero.distancia_vertical = total_alto * 0.05
-  for fila=1, 3 do
-    for colum=1, 3 do
-      local celda_posicion_x = tablero.arriba_izquierda_x +
-	   (colum-1) * ( tablero.ancho + tablero.distancia_horizontal )
-	  local celda_posicion_Y = tablero.arriba_izquierda_y +
-	   (fila-1) * ( tablero.alto + tablero.distancia_vertical )
-	  local nueva_celda = tablero.nueva_celda( celda_posicion_x, celda_posicion_Y, fila, colum )
-	  table.insert( tablero.celdas, nueva_celda )
-    end
-   end
-end
+local tablero = require "tablero"
 
-function tablero.draw() 
-  for _, celda in pairs( tablero.celdas ) do
-    love.graphics.rectangle( 'line', celda.pos_x, celda.pos_y, celda.ancho, celda.alto )
-  end
-end
+-- Quien juega?
+Jugador_Actual=1
 
 -- General
 function love.load()
+  total_ancho = love.graphics.getWidth()
+  total_alto = love.graphics.getHeight()
+  love.graphics.setBackgroundColor(140, 101, 110, 255)
   tablero.load()
 end
 
 function love.draw()
-  tablero.draw()
+  local x, y = love.mouse.getPosition()
+  tablero.draw(x,y)
+  love.graphics.print("TicTacToe Deluxe Edition",love.graphics.getHeight() / 2 + 10 ,10)
+  love.graphics.print(mensaje,love.graphics.getHeight() / 2 + 10 ,25)
+end
+
+function love.update(dt)
+  tablero.update(dt)
+end
+
+-- Marcar celda
+function love.mousepressed( x, y, button, istouch )
+   if button == 'l' or button == 1 then
+     local celda = tablero.Celda_Tocada( x, y )
+     if celda ~= nil then
+	   tablero.Celda_Marcar(celda)
+	 end
+   end    
 end
 
 -- Salir con escape
@@ -57,6 +47,7 @@ function love.keypressed(key)
       love.event.quit()
    end
 end
+
 function love.quit()
   print("Thanks for playing! Come back soon!")
 end
